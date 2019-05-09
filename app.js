b99b0ff5805a2373889a1abac80c67c5349c34c9
Uğurcan Eser <ugurcaneser@macbook-ugurcan.local>
2019-05-09 15:35:09 +0300

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+let vue = new Vue({
+  el: '#app',
+  data: {
+    title: "Test Header",
+    buttonTitle: "Click & Go!",
+    paragraph: "Lorom ipsum...",
+    link: 'https://loremipsum.io/',
+    dataId: "1"
+  }
+});
+
+let vue2 = new Vue({
+  el: '#app2',
+  data: {
+    seen: true
+  }
+});
