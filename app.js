3547f848c8330d0f3a78fd00ff7aec1c2160c29f
Uğurcan Eser <ugurcaneser@macbook-ugurcan.local>
2019-05-13 15:59:25 +0300

    last changes

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-let vue = new Vue({
-  el: '#app',
-  data: {
-    title: "Test Header",
-    buttonTitle: "Click & Go!",
-    paragraph: "Lorom ipsum...",
-    link: 'https://loremipsum.io/',
-    dataId: "1"
-  }
-});
-
-let vue2 = new Vue({
-  el: '#app2',
-  data: {
-    seen: true
-  }
+new Vue({
+    el: "#app",
+    data: {
+        counter: 0
+    },
+    methods: {
+        reset: function() {
+            return this.counter =  0;
+            
+        },
+        result: function() {
+        
+            if (this.counter == 0) {
+                return "Equal to 0"
+            } else if (this.counter > 0) {
+                return "Greater than 0"
+            } else if (this.counter < 0) {
+                return "Lesser than 0"
+            }
+        }
+    }
 });
