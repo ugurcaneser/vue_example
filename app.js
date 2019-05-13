70b732615251b9f1bd7d1d28d75204e42ff31f77
Uğurcan Eser <ugurcaneser@macbook-ugurcan.local>
2019-05-13 16:03:30 +0300

    last changes

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ new Vue({
     methods: {
         reset: function() {
             return this.counter =  0;
-            
         },
         result: function() {
-        
-            if (this.counter == 0) {
-                return "Equal to 0"
+            if (this.counter < 0) {
+                return "Lesser than 0";
+            } else if (this.counter == 0) {
+                return "Equal to 0";
             } else if (this.counter > 0) {
-                return "Greater than 0"
-            } else if (this.counter < 0) {
-                return "Lesser than 0"
+                return "Greater than 0";
             }
         }
     }
